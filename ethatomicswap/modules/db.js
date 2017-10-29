9b6541630fce9e04ee4a349f422b2200b5c1f315
MisterDr <djenadrazic@gmail.com>
2017-10-29 14:59:39 +0100

    - Added Subscribe and Find commands to Db engine

--- ethatomicswap/modules/db.js
+++ ethatomicswap/modules/db.js
@@ -1,4 +1,5 @@
 const driver = require('bigchaindb-driver');
+const WebSock = global.WebSocket || global.MozWebSocket || require('ws');
 
 /**
  * Atomic swap class
@@ -10,26 +11,58 @@ var Db = function (configuration) {
     this.config = configuration;
     this.kp = new driver.Ed25519Keypair();
     this.conn = null;
+    this.ws = null;
 
     /**
      * Send data to DB
      * @returns {Promise}
      * @constructor
      */
-    this.Send = function(data) {
+    this.Send = function(data, callbackPost) {
         that = this;
         var tx = driver.Transaction.makeCreateTransaction(
             data,
             null,
             [ driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(this.kp.publicKey))], this.kp.publicKey);
         var txSigned = driver.Transaction.signTransaction(tx, this.kp.privateKey);
-        this.conn.postTransaction(txSigned).then(function (result) {
-            that.conn.pollStatusAndFetchTransaction(result.id).then(function(e, r) {
-                console.log(e);
-            });
-        }).then(function (result, res) {
-            console.log(result);
+        this.conn.postTransaction(txSigned).then(callbackPost);
+    };
+
+    /**
+     * Get data from transaction id
+     * @param id
+     * @constructor
+     */
+    this.Get = function(id) {
+        return this.conn.pollStatusAndFetchTransaction(id);
+    };
+
+    /**
+     * Find data using MongoDB filter (only supported by now)
+     * @param data
+     * @constructor
+     */
+    this.Find = function(data) {
+        return this.conn.searchAssets(data);
+    };
+
+    /**
+     * Subscribe to transaction feed
+     * @param callback events
+     */
+    this.Subscribe = function(callback) {
+
+        this.ws = new WebSocket(this.config.wshost, {});
+
+        this.ws.on('open', function open() {
+            console.log('Connected!');
         });
+
+        this.ws.on('close', function close() {
+            console.log('Disconnected!');
+        });
+
+        this.ws.on('message', callback);
     };
 
     /**
