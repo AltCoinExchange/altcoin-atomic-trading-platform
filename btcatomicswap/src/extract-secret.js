b9289a85d71856b8fd3b22a9054873c2f64dfbbd
Admir Sabanovic <admir_sabanovic@live.com>
2017-11-01 20:06:33 +0100

    extract secret fix and add test

--- btcatomicswap/src/extract-secret.js
+++ btcatomicswap/src/extract-secret.js
@@ -1,25 +1,22 @@
-import {hash160} from './common/secret-hash';
-
+import {hash160, ripemd160} from './common/secret-hash';
+const buffer = require('buffer');
 const Transaction = require('bitcore').Transaction;
 const Script = require('bitcore').Script;
 
 
 export const extractSecret = (redemptionTx, secretHash) => {
   const transaction = new Transaction(redemptionTx);
-
   let secret;
   transaction.toJSON().inputs.some(input => {
     const script = new Script(input.scriptString);
     const pops = script.toString().split(' ');
     const data = pops.filter(opcode => opcode.indexOf('0x') !== -1).map(opdata => opdata.replace('0x', ''));
-
     const sc = data.find((d) => {
-      return hash160(Buffer.from(d, "hex")) === secretHash;
+      return ripemd160(buffer.Buffer.from(d, "hex")) === secretHash;
     });
     secret = sc;
 
     return sc;
   });
-  console.log(secret);
   return secret;
 };
