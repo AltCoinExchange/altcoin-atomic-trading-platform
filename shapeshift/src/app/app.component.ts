93f702f8264c528c030e80b6773743e1ab15677a
Admir Sabanovic <admir_sabanovic@live.com>
2017-11-03 00:11:16 +0100

    removed unused code

--- shapeshift/src/app/app.component.ts
+++ shapeshift/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit, ViewEncapsulation,} from '@angular/core';
 
 import * as btcswap from 'btc-atomic-swap';
-import {Headers, Http, RequestOptions} from '@angular/http';
 
 @Component({
   selector: 'app',
@@ -16,22 +15,8 @@ export class AppComponent implements OnInit {
   public name = 'Angular 2 Webpack Starter';
   public url = 'https://twitter.com/AngularClass';
 
-  constructor(private http: Http) {
-    console.log(btcswap);
-    const options = new RequestOptions();
-    options.headers = new Headers();
-    options.headers.set('Authorization', 'Basic ' + window.btoa('bedrock:bedrock'));
-
-    // this.http.post('http://bedrock:bedrock@34.215.58.192:8013/', {
-    //   'user': 'bedrock',
-    //   'password': 'bedrock',
-    //   'method': 'estimatesmartfee',
-    //   'params': [6],
-    //   'auth': 'bedrock:bedrock'
-    // }, options).subscribe(r => {
-    //   console.log(r);
-    // });
-    btcswap.initiate("n31og5QGuS28dmHpDH6PQD5wmVQ2K2spAG", "0.01");
+  constructor() {
+    btcswap.initiate('n31og5QGuS28dmHpDH6PQD5wmVQ2K2spAG', '0.01');
   }
 
   public ngOnInit() {
