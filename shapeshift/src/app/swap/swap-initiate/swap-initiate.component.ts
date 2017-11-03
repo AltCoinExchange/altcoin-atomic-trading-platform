1e14b4b3168f711db0e9905f150633515b014967
Admir Sabanovic <admir_sabanovic@live.com>
2017-11-03 11:43:35 +0100

    extract initiate swap component

---
+++ shapeshift/src/app/swap/swap-initiate/swap-initiate.component.ts
@@ -0,0 +1,54 @@
+import {Component, HostBinding, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {SwapProcess} from '../../models/swap-process.model';
+import {Coin} from '../../models/coin.model';
+import {flyInOutAnimation} from '../../animations/animations';
+
+@Component({
+  selector: 'app-swap-initiate',
+  templateUrl: './swap-initiate.component.html',
+  styleUrls: ['./swap-initiate.component.scss'],
+  animations: [flyInOutAnimation],
+})
+export class SwapInitiateComponent implements OnInit {
+  @HostBinding('@flyInOut') state = 'in';
+  @HostBinding('style.display') display = 'block';
+  @HostBinding('class') classes = 'swap';
+
+  eth: Coin = {
+    name: 'ETH',
+    amount: 0,
+    animationSwapState: 'slideBack',
+    icon: 'assets/icon/eth-icon.png',
+    iconOutline: 'assets/icon/eth-icon-o.png',
+  };
+  btc: Coin = {
+    name: 'BTC',
+    amount: 0,
+    animationSwapState: 'slideBack',
+    icon: 'assets/icon/btc-icon.png',
+    iconOutline: 'assets/icon/btc-icon-o.png',
+  };
+  swapProcess: SwapProcess = {
+    depositCoin: this.eth,
+    receiveCoin: this.btc,
+    submitAmount: false,
+    showQRCode: false,
+    showLink: false,
+  };
+  firstSpinner = true;
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+  }
+
+  doSubmit() {
+    this.state = 'void';
+    setTimeout(() => {
+      this.router.navigate(['/insufficient-amount']);
+    }, 500);
+  }
+
+}
--- shapeshift/src/app/swap/swap.module.ts
+++ shapeshift/src/app/swap/swap.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
-import {SwapComponent} from './swap.component';
 import {ShapeShiftCommonModule} from '../common/common.module';
 import {SwapIconComponent} from './swap-icon/swap-icon.component';
 import {QrCodeComponent} from './qr-code/qr-code.component';
 import {SwapContainerComponent} from './swap-container/swap-container.component';
+import {SwapInitiateComponent} from './swap-initiate/swap-initiate.component';
 
 @NgModule({
   imports: [
@@ -14,7 +14,7 @@ import {SwapContainerComponent} from './swap-container/swap-container.component'
       {
         path: '', component: SwapContainerComponent, children: [
         {path: '', redirectTo: 'swap', pathMatch: 'full'},
-        {path: 'swap', component: SwapComponent},
+        {path: 'swap', component: SwapInitiateComponent},
         {path: 'insufficient-amount', component: QrCodeComponent},
       ],
       },
@@ -22,10 +22,10 @@ import {SwapContainerComponent} from './swap-container/swap-container.component'
     ShapeShiftCommonModule,
   ],
   declarations: [
-    SwapComponent,
     SwapIconComponent,
     QrCodeComponent,
     SwapContainerComponent,
+    SwapInitiateComponent,
   ],
 })
 export class SwapModule {
