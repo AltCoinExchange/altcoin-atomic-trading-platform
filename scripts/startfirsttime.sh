9e6a5fd64d1218e1ccd8b20e05808827f9ba933a
MisterDr <djenadrazic@gmail.com>
2017-11-04 22:10:44 +0100

    - Added fixes for first time run script

--- scripts/startfirsttime.sh
+++ scripts/startfirsttime.sh
@@ -2,14 +2,16 @@
 cd btcatomicswap
 yarn
 yarn build
-cd ../wallet
+cd node_modules
+find . -name '*.js' -type f -print0 | xargs -0 sed -i "" "s/bitcore\.versionGuard = function\(version\) \{/bitcore\.versionGuard = function\(version\) \{ return true;/g"
+cd ../../wallet
 yarn
 cd node_modules
-find . -name '*.js' -print0 | xargs -0 sed -i "" "s/bitcore.versionGuard = function(version) {/bitcore.versionGuard = function(version) { return true;/g"
+find . -name '*.js' -type f  -print0 | xargs -0 sed -i "" "s/bitcore.versionGuard = function(version) {/bitcore.versionGuard = function(version) { return true;/g"
 cd ../../shapeshift/
 yarn
 cd ..
 ./scripts/linkallfirsttime.sh
 find shapeshift/node_modules/@angular/cli/models/webpack-configs/common.js -print0 | xargs -0 sed -i "" "s/crypto: 'empty'/crypto: true/g"
 cd shapeshift
-ng serve
+yarn start
