41e4be4ad06f7f34e31d46a3ff9e78be64385226
AntoBrajkovic <antobrajkovic@gmail.com>
2017-11-04 14:16:36 +0100

    redeem works needs refactoring

--- btcatomicswap/src/redeem.js
+++ btcatomicswap/src/redeem.js
@@ -10,6 +10,9 @@ import {createSig} from './common/createSig';
 import {hash160} from './common/secret-hash';
 import {AddressUtil} from './common/address-util';
 const strPubKey = "03b10e3690bcaf0eae7098ec794666963803bcec5acfbe6a112bc8cdc93797f002"
+import {configuration} from "./config/config"
+import {publishTx} from "./common/public-tx.js"
+import {getFeePerKb} from './common/fee-per-kb';
 
 
 const util = require('util');
@@ -17,7 +20,15 @@ const util = require('util');
 
 export async function redeem(strCt, strCtTx, secret) {
 
+  // "contract is not an atomic swap script recognized by this tool"
+  // "transaction does not contain a contract output"
+  // "getrawchangeaddres" + erroe
+  // "redeem output value of %v is dust"
+  // "Redeem fee: %v (%0.8f BTC/kB)\n\n"
+  // "Redeem transaction (%v):\n"
 
+  // TODO: clean the tempInput, reedemTxTemp
+  // TODO: change strCt, strCtTx to ct, ctTx
   const contract = new Script(strCt);
   const pushes = extractAtomicSwapContract(strCt)
 
@@ -25,91 +36,82 @@ export async function redeem(strCt, strCtTx, secret) {
 
   const recipientAddrString = pushes.recipientHash.replace('0x', '');
   const recipientAddress = AddressUtil.NewAddressPubKeyHash(recipientAddrString, 'testnet');
+  const contractP2SH = AddressUtil.NewAddressScriptHash(strCt, configuration.network);
 
-  const contractSH = AddressUtil.NewAddressScriptHash(strCt, 'testnet').toString();
-
-  const contractScriptHashOut = contract.toScriptHashOut();
-  const contractAddress = contractScriptHashOut.toAddress();
-  const contractAddressString = contractScriptHashOut.toAddress().toJSON().hash;
-
-  // console.log(ctTx.outputs.length);
-  // console.log(ctTx.outputs[0]);
-  // console.log(ctTx.outputs[0].script);
   let ctTxOutIdx = -1
+
 
   for(let i = 0; i<ctTx.outputs.length; i ++ ){
     const script = new Script(ctTx.outputs[i].script)
     const address = script.toAddress(configuration.network)
     const addressHash = address.toJSON().hash;
 
-
-    // TODO: implement a check to see if its a p2sh and then check the address
-    if (addressHash === contractAddressString){
+    if (addressHash === contractP2SH.toJSON().hash){
       ctTxOutIdx = i
       break
     }
   }
-  console.log(pushes.recipientHash);
 
-  console.log(ctTxOutIdx);
-  // let addr = await getChangeAddress()
+  if (ctTxOutIdx == -1) {
+    return "transaction does not contain a contract output"
+  }
+
+
   const addr = new Address(await getChangeAddress())
-  console.log(addr);
   const outScript = Script.buildPublicKeyHashOut(addr)
-  console.log(outScript);
+  const amount = ctTx.outputs[ctTxOutIdx].satoshis - 0.0005*100000000
 
-  // TODO:
-  // console.log(ctTx.outputs[ctTxOutIdx].satoshis);
-  const amount = ctTx.outputs[ctTxOutIdx].satoshis
-  console.log(amount);
+  // https://bitcoin.org/en/developer-examples#offline-signing
   const reedemTx = new Transaction()
-  console.log(reedemTx);
+  const reedemTxTemp = new Transaction()
+
   let output = Transaction.Output({
     script: outScript,
     satoshis: amount
   })
-  reedemTx.addOutput(output)
-  console.log(reedemTx);
+  reedemTxTemp.addOutput(output)
   // createSig(recipientAddress)
   console.log("---------");
   // console.log(Transaction.Sighash);
-  const tAddr = "n4Fc4SbP7tqhCVch1eVS8sj9E919X1SUqS";
-  // const script = redeemP2SHContract(strContract, strSig, strPubkey, strSecret);
-  //
-  const input = Transaction.Input({
+  console.log("**addr", addr);
+
+  const tAddr = "mhv5J1ymh9Hd5tk1YuKvmXCbV63whjyxmT";
+
+  const tempInput = Transaction.Input({
     prevTxId: ctTx.id,
     outputIndex: ctTxOutIdx,
     script: new Script(ctTx.outputs[ctTxOutIdx].script)
   })
 
-  console.log(input);
-  console.log(reedemTx.uncheckedAddInput(input));
-  const {sig, pubKey} = await createSig(reedemTx, 0, contract, tAddr )
-  // const obj = await createSig(reedemTx, 0, contract, tAddr )
-  // console.log(obj);
-  console.log(sig);
-  console.log(pubKey);
-  //
-  console.log("**strCt  ", strCt);
-  console.log("**Ct  ", contract.toHex());
-  console.log("**strCtTx  ", strCtTx);
-  console.log("**CtTx  ", ctTx);
-  console.log("**Pubkey  ", pubKey.toString());
-  // console.log("**strPubkey  ", strPubkey);
-  console.log("**secret  ", secret);
-  console.log("**verify  ", sig);
-  console.log("**sig  ", sig.toString())
-
-  // const script = redeemP2SHContract(strCt, sig, pubKey.toString(), secret);
-
-  // check the contract
-  // return errors.New("contract is not an atomic swap script recognized by this tool")
+  console.log(reedemTxTemp.uncheckedAddInput(tempInput));
+  const {sig, pubKey} = await createSig(reedemTxTemp, 0, contract, recipientAddress )
 
 
 
-  // check if the contractTx containes the contract
-  // and return the index of the contract
-  // return errors.New("transaction does not contain a contract output")
+  // const script = redeemP2SHContract(strCt, sig.toString(), pubKey.toString(), secret);
+  const script = redeemP2SHContract(contract.toHex(), sig.toTxFormat(), pubKey.toString(), secret);
+  console.log(script);
+
+
+  const input = Transaction.Input({
+    prevTxId: ctTx.id,
+    outputIndex: ctTxOutIdx,
+    script: script
+  })
+  // reedemTx.addInput(input, outScript, amount)
+  reedemTx.uncheckedAddInput(input)
+  reedemTx.addOutput(output)
+  // console.log(reedemTxTemp.inputs[0].setScript(script));
+  console.log(reedemTxTemp);
+  console.log("**reedemTx  ", reedemTx.toString());
+  console.log("**reedemTx  ", reedemTx);
+  console.log(reedemTx.verify());
+  const res = await publishTx(reedemTx.toString())
+  // const res = await publishTx(reedemTx)
+  console.log(res);
+
+
+  // return reedemTx
 
 }
 
