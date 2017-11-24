bef482cbc2977882b72728001178f7c150703ffd
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2017-11-24 16:48:42 +0100

    side b effects

--- shapeshift/src/app/effects/side-B.effect.ts
+++ shapeshift/src/app/effects/side-B.effect.ts
@@ -1,25 +1,110 @@
 import {Injectable} from "@angular/core";
+import {Actions, Effect} from "@ngrx/effects";
+import {Action} from "@ngrx/store";
+import {Observable} from "rxjs/Observable";
+import * as sideB from "../actions/side-B.action";
 
 @Injectable()
 export class SideBEffect {
-  $initiate: any;
-  $initiateSuccess: any;
 
-  $informInitiate: any;
-  $informInitiateSuccess: any;
+  @Effect()
+  $initiate: Observable<Action> = this.actions$
+    .ofType(sideB.INITIATE)
+    .mergeMap(() => {
+        return Observable.empty().map(resp => { // TODO provide implementation
+          return new sideB.InitiateSuccessAction(resp);
+        }).catch(err => Observable.of(new sideB.InitiateFailAction(err)));
+      },
+    );
 
-  $waitForParticipate: any;
-  $waitForParticipateSuccess: any;
+  @Effect()
+  $initiateSuccess: Observable<Action> = this.actions$
+    .ofType(sideB.INITIATE_SUCCESS)
+    .mergeMap(() => {
+      return Observable.empty().map(resp => { // TODO provide implementation
+        return new sideB.InformInitiateAction(resp);
+      });
+    });
 
-  $redeem;
-  $redeemSuccess;
+  @Effect()
+  $informInitiate: Observable<Action> = this.actions$
+    .ofType(sideB.INFORM_INITIATE)
+    .mergeMap(() => {
+        return Observable.empty().map(resp => { // TODO provide implementation
+          return new sideB.InformInitiateSuccessAction(resp);
+        }).catch(err => Observable.of(new sideB.InformInitiateFailAction(err)));
+      },
+    );
 
-  $informRedeemed: any;
-  $informRedeemedSuccess: any;
+  @Effect()
+  $informInitiateSuccess: Observable<Action> = this.actions$
+    .ofType(sideB.INFORM_INITIATE_SUCCESS)
+    .mergeMap(() => {
+      return Observable.empty().map(resp => { // TODO provide implementation
+        return new sideB.WaitForParticipateAction(resp);
+      });
+    });
+
+  @Effect()
+  $waitForParticipate: Observable<Action> = this.actions$
+    .ofType(sideB.WAIT_FOR_PARTICIPATE)
+    .mergeMap(() => {
+        return Observable.empty().map(resp => { // TODO provide implementation
+          return new sideB.WaitForParticipateSuccessAction(resp);
+        }).catch(err => Observable.of(new sideB.WaitForParticipateFailAction(err)));
+      },
+    );
+
+  @Effect()
+  $waitForParticipateSuccess: Observable<Action> = this.actions$
+    .ofType(sideB.WAIT_FOR_PARTICIPATE_SUCCESS)
+    .mergeMap(() => {
+      return Observable.empty().map(resp => { // TODO provide implementation
+        return new sideB.BRedeemAction(resp);
+      });
+    });
+
+  @Effect()
+  $redeem: Observable<Action> = this.actions$
+    .ofType(sideB.BREDEEM)
+    .mergeMap(() => {
+        return Observable.empty().map(resp => { // TODO provide implementation
+          return new sideB.BRedeemSuccessAction(resp);
+        }).catch(err => Observable.of(new sideB.BRedeemFailAction(err)));
+      },
+    );
+
+  @Effect()
+  $redeemSuccess: Observable<Action> = this.actions$
+    .ofType(sideB.BREDEEM_SUCCESS)
+    .mergeMap(() => {
+      return Observable.empty().map(resp => { // TODO provide implementation
+        return new sideB.InformRedeemedAction(resp);
+      });
+    });
+
+  @Effect()
+  $informRedeemed: Observable<Action> = this.actions$
+    .ofType(sideB.INFORM_REDEEMED)
+    .mergeMap(() => {
+        return Observable.empty().map(resp => { // TODO provide implementation
+          return new sideB.InformRedeemedSuccessAction(resp);
+        }).catch(err => Observable.of(new sideB.InformRedeemedFailAction(err)));
+      },
+    );
+
+  @Effect()
+  $informRedeemedSuccess: Observable<Action> = this.actions$
+    .ofType(sideB.INFORM_REDEEMED_SUCCESS)
+    .mergeMap(() => {
+      return Observable.empty().map(resp => { // TODO provide implementation
+        return new sideB.BDoneAction(resp);
+      });
+    });
 
   $done;
 
-  constructor() {
+  constructor(private actions$: Actions) {
 
   }
 }
