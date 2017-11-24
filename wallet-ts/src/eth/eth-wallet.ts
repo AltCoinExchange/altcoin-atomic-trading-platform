be31ef45568bb7350ddd7104ec308b6f6c20f55d
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2017-11-24 13:59:44 +0100

    fix tests

--- wallet-ts/src/eth/eth-wallet.ts
+++ wallet-ts/src/eth/eth-wallet.ts
@@ -9,4 +9,23 @@ export class EthWallet extends EthAtomicSwap {
     super(AtomicSwapAbi, AppConfig.EthConfiguration.hosts[0], AtomicSwapBin);
   }
 
+  public login(keystore, password) {
+    return this.engine.login(keystore, password);
+  }
+
+  public create(password): IEthAccount {
+    return this.engine.createAccount(password);
+  }
+
+  public recover(privateKey, password) {
+    return this.engine.recoverAccount(privateKey, password);
+  }
+
+  public async getbalance(address) {
+    return await this.engine.getBalance(address);
+  }
+
+  public sendAllEther(privateKey, toAddress) {
+    return this.engine.sendAllEther(privateKey, toAddress);
+  }
 }
