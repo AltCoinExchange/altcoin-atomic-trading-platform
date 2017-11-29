a08d109f53e198353ac0c571b0a4ec0faa580159
MisterDr <djenadrazic@gmail.com>
2017-11-29 18:27:08 +0100

    - Changed startfirsttime.sh script

--- scripts/startfirsttime.sh
+++ scripts/startfirsttime.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd wallet-ts
+cd wallet
 npm install
 cd node_modules
 find . -name '*.js' -type f -print0 | xargs -0 sed -i '' -e "s/bitcore.versionGuard = function(version) {/bitcore.versionGuard = function(version) { return true;/g"
@@ -8,7 +8,7 @@ npm run build
 cd ../../shapeshift/
 npm install
 cd node_modules
-ln -s wallet-ts ts-wallet
+ln -s wallet altcoinio-wallet
 find @angular/cli/models/webpack-configs/common.js -print0 | xargs -0 sed -i '' -e "s/crypto: 'empty'/crypto: true/g"
 cd ..
 npm run start
