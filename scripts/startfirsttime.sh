b2130f5d114b360aaa95cf29d88e96f4172713e3
MisterDr <djenadrazic@gmail.com>
2017-11-29 18:54:44 +0100

    - fixed module linking

--- scripts/startfirsttime.sh
+++ scripts/startfirsttime.sh
@@ -4,11 +4,12 @@ cd wallet
 npm install
 cd node_modules
 find . -name '*.js' -type f -print0 | xargs -0 sed -i '' -e "s/bitcore.versionGuard = function(version) {/bitcore.versionGuard = function(version) { return true;/g"
+cd ..
 npm run build
-cd ../../shapeshift/
+cd ../shapeshift/
 npm install
 cd node_modules
-ln -s wallet altcoinio-wallet
+ln -s ../../wallet altcoinio-wallet
 find @angular/cli/models/webpack-configs/common.js -print0 | xargs -0 sed -i '' -e "s/crypto: 'empty'/crypto: true/g"
 cd ..
 npm run start
