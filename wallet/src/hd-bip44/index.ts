15e2641dd1b1f75a04866cec1d10e28fd40f01eb
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2017-12-11 20:56:06 +0100

    added links

---
+++ wallet/src/hd-bip44/index.ts
@@ -0,0 +1,5 @@
+// https://github.com/satoshilabs/slips/blob/master/slip-0044.md
+// https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki
+// https://github.com/bitcoinjs/bitcoinjs-lib
+// https://github.com/bitcoinjs/bitcoinjs-lib/blob/master/test/integration/bip32.js#L50
+// https://github.com/bitcoinjs/bip44-constants
