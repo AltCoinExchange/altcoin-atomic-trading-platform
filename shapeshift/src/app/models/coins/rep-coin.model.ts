a21b9ba90e3f709fd2f5768142fab077ebaca886
MisterDr <djenadrazic@gmail.com>
2017-12-12 21:03:51 +0100

    - Added getting balance from token at UI

---
+++ shapeshift/src/app/models/coins/rep-coin.model.ts
@@ -0,0 +1,20 @@
+import {Coin} from "./coin.model";
+import {Coins} from "./coins.enum";
+
+export class EthCoinModel implements Coin {
+  readonly type: Coins = Coins.ETH;
+  readonly name: string = Coins[Coins.ETH].toString();
+  readonly icon: string = "assets/icon/eth-icon.png";
+  iconOutline: string = "assets/icon/eth-icon-o.png";
+  readonly fullName: string = "Ethereum";
+  amount: number = 0;
+
+  constructor() {
+  }
+
+  update(coin: EthCoinModel): EthCoinModel {
+    const model = new EthCoinModel();
+    model.amount = coin ? coin.amount : 0;
+    return model;
+  }
+}
