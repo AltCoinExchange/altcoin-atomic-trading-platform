6ac87984ed99756ffabf09b10cecec5f0a4b6f55
MisterDr <djenadrazic@gmail.com>
2017-12-14 12:28:51 +0100

    - Added all token states

--- shapeshift/src/app/wallet/wallet.component.ts
+++ shapeshift/src/app/wallet/wallet.component.ts
@@ -10,9 +10,11 @@ import {MessageTypes} from "../models/message-types.enum";
 import {AppState} from "../reducers/app.state";
 import {WalletRecord} from "../reducers/balance.reducer";
 import {
-  getBTCBalance, getBtcLoading, getETHBalance, getEthLoading, getREPBalance, getTokenBalanceAugur, getTokenBalanceGolem,
-  getTokenLoadingAugur,
-  getTokenLoadingGolem,
+  getBTCBalance, getBtcLoading, getETHBalance, getEthLoading, getREPBalance, getTokenBalanceAragon,
+  getTokenBalanceAugur, getTokenBalanceBat, getTokenBalanceEos, getTokenBalanceGnosis, getTokenBalanceGolem,
+  getTokenBalanceSalt, getTokenLoadingAragon,
+  getTokenLoadingAugur, getTokenLoadingBat, getTokenLoadingEos, getTokenLoadingGnosis,
+  getTokenLoadingGolem, getTokenLoadingSalt,
 } from "../selectors/balance.selector";
 
 @Component({
@@ -30,53 +32,71 @@ export class WalletComponent implements OnInit {
   sendAmount: Number = 0.00;
   $ethLoading: Observable<boolean>;
   $btcLoading: Observable<boolean>;
-  //$repLoading: Observable<boolean>;
   $tokenAugurLoading: Observable<boolean>;
   $tokenGolemLoading: Observable<boolean>;
+  $tokenEosLoading: Observable<boolean>;
+  $tokenSaltLoading: Observable<boolean>;
+  $tokenBatLoading: Observable<boolean>;
+  $tokenGnosisLoading: Observable<boolean>;
+  $tokenAragonLoading: Observable<boolean>;
   $tokenBalanceAugur: Observable<WalletRecord>;
   $tokenBalanceGolem: Observable<WalletRecord>;
+  $tokenBalanceAragon: Observable<WalletRecord>;
+  $tokenBalanceEos: Observable<WalletRecord>;
+  $tokenBalanceSalt: Observable<WalletRecord>;
+  $tokenBalanceBat: Observable<WalletRecord>;
+  $tokenBalanceGnosis: Observable<WalletRecord>;
   $ethBalance: Observable<WalletRecord>;
   $btcBalance: Observable<WalletRecord>;
-  //$repBalance: Observable<WalletRecord>;
 
   constructor(private store: Store<AppState>) {
     this.infoMsg = "This wallet is to be used for testnet coins only. Do not send real Bitcoin or Ethereum to these addresses.";
 
     this.store.dispatch(new GetEthBalanceAction());
     this.store.dispatch(new GetBtcBalanceAction());
-    //this.store.dispatch(new GetRepBalanceAction());
 
     this.store.dispatch(new GetTokenBalanceAction({ token: TOKENS.GOLEM, name: "golem" }));
     this.store.dispatch(new GetTokenBalanceAction({ token: TOKENS.AUGUR, name: "augur" }));
-
-    // this.$ethLoading = this.store.select(getETHBalance).flatMap((e) => { return Observable.of(e.loading)});
-    // this.$btcLoading = this.store.select(getBTCBalance).flatMap((e) => { return Observable.of(e.loading)});
+    this.store.dispatch(new GetTokenBalanceAction({ token: TOKENS.ARAGON, name: "aragon" }));
+    this.store.dispatch(new GetTokenBalanceAction({ token: TOKENS.BAT, name: "bat" }));
+    this.store.dispatch(new GetTokenBalanceAction({ token: TOKENS.EOS, name: "eos" }));
+    this.store.dispatch(new GetTokenBalanceAction({ token: TOKENS.GNOSIS, name: "gnosis" }));
+    this.store.dispatch(new GetTokenBalanceAction({ token: TOKENS.SALT, name: "salt" }));
 
     this.$btcLoading = this.store.select(getBtcLoading);
     this.$ethLoading = this.store.select(getEthLoading);
     this.$tokenAugurLoading = this.store.select(getTokenLoadingAugur);
     this.$tokenGolemLoading = this.store.select(getTokenLoadingGolem);
+    this.$tokenAragonLoading = this.store.select(getTokenLoadingAragon);
+    this.$tokenBatLoading = this.store.select(getTokenLoadingBat);
+    this.$tokenEosLoading = this.store.select(getTokenLoadingEos);
+    this.$tokenGnosisLoading = this.store.select(getTokenLoadingGnosis);
+    this.$tokenSaltLoading = this.store.select(getTokenLoadingSalt);
 
     this.$ethBalance = this.store.select(getETHBalance);
     this.$btcBalance = this.store.select(getBTCBalance);
-    //this.$repBalance = this.store.select(getREPBalance);
     this.$tokenBalanceAugur = this.store.select(getTokenBalanceAugur);
     this.$tokenBalanceGolem = this.store.select(getTokenBalanceGolem);
+    this.$tokenBalanceAragon = this.store.select(getTokenBalanceAragon);
+    this.$tokenBalanceBat = this.store.select(getTokenBalanceBat);
+    this.$tokenBalanceEos = this.store.select(getTokenBalanceEos);
+    this.$tokenBalanceGnosis = this.store.select(getTokenBalanceGnosis);
+    this.$tokenBalanceSalt = this.store.select(getTokenBalanceSalt);
 
 
     this.walletOptions = [{id: 0, name: 'SEND'},{ id: 1, name: 'RECEIVE'},{ id: 2, name: 'TRANSACTIONS'}];
-    this.wallets=[
+    this.wallets = [
       { id: 0, icon: 'assets/icon/eth-icon-o.png', name: 'ETH', fullName: 'Ethereum' ,$balance: this.$ethBalance, usd: 0, $loading: this.$ethLoading },
       { id: 1, icon: 'assets/icon/btc-icon-o.png', name: 'BTC', fullName: 'Bitcoin', $balance: this.$btcBalance, usd: 0, $loading: this.$btcLoading },
       { id: 2, icon: 'assets/icon/ltc-icon-o.png', name: 'LTC', fullName: 'Litecoin', usd: 0, $loading: this.$ethLoading },
       { id: 3, icon: 'assets/icon/dcr-icon-o.png', name: 'DCR', fullName: 'Decred', usd: 0, $loading: this.$ethLoading },
       { id: 4, icon: 'assets/icon/rep-icon-o.png', name: 'REP', fullName: 'Augur', $balance: this.$tokenBalanceAugur, usd: 0, $loading: this.$tokenAugurLoading },
       { id: 5, icon: 'assets/icon/gnt-icon-o.png', name: 'GNT', fullName: 'Golem', $balance: this.$tokenBalanceGolem, usd: 0, $loading: this.$tokenGolemLoading },
-      { id: 6, icon: 'assets/icon/gno-icon-o.png', name: 'GNO', fullName: 'Gnosis', usd: 0, $loading: this.$ethLoading },
-      { id: 7, icon: 'assets/icon/bat-icon-o.png', name: 'BAT', fullName: 'BAT', usd: 0, $loading: this.$ethLoading },
-      { id: 8, icon: 'assets/icon/ant-icon-o.png', name: 'ANT', fullName: 'Aragon', usd: 0, $loading: this.$ethLoading },
-      { id: 9, icon: 'assets/icon/eos-icon-o.png', name: 'EOS', fullName: 'EOS', usd: 0, $loading: this.$ethLoading },
-      { id: 10, icon: 'assets/icon/salt-icon-o.png', name: 'SALT', fullName: 'SALT', usd: 0, $loading: this.$ethLoading },
+      { id: 6, icon: 'assets/icon/gno-icon-o.png', name: 'GNO', fullName: 'Gnosis', $balance: this.$tokenBalanceGnosis, usd: 0, $loading: this.$tokenGnosisLoading },
+      { id: 7, icon: 'assets/icon/bat-icon-o.png', name: 'BAT', fullName: 'BAT', $balance: this.$tokenBalanceBat, usd: 0, $loading: this.$tokenBatLoading },
+      { id: 8, icon: 'assets/icon/ant-icon-o.png', name: 'ANT', fullName: 'Aragon', $balance: this.$tokenBalanceAragon, usd: 0, $loading: this.$tokenAragonLoading },
+      { id: 9, icon: 'assets/icon/eos-icon-o.png', name: 'EOS', fullName: 'EOS', $balance: this.$tokenBalanceEos, usd: 0, $loading: this.$tokenEosLoading },
+      { id: 10, icon: 'assets/icon/salt-icon-o.png', name: 'SALT', fullName: 'SALT', $balance: this.$tokenBalanceSalt, usd: 0, $loading: this.$tokenSaltLoading },
     ];
   }
 
