ee9fe1967b30f5833fe44732194c139c9de26eb5
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-01-03 21:23:05 +0100

    uncomment half of app

--- shapeshift/src/app/app-routing.module.ts
+++ shapeshift/src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import {NgModule} from "@angular/core";
 import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
 
 export const ROUTES: Routes = [
-  // {
-  //   path: "", loadChildren: "app/swap/swap.module#SwapModule",
-  // },
+  {
+    path: "", loadChildren: "app/swap/swap.module#SwapModule",
+  },
   {
     path: "wallet", loadChildren: "app/wallet/wallet.module#WalletModule"
   },
-  // {
-  //   path: "links", loadChildren: "app/links/links.module#LinksModule"
-  // }
+  {
+    path: "links", loadChildren: "app/links/links.module#LinksModule"
+  }
 ];
 
 @NgModule({
