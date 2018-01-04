19184986d90248820e73ea5819e171a416eb3f77
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-01-04 13:58:22 +0100

    adads

--- wallet/fix_scripts/fix_bitcore.sh
+++ wallet/fix_scripts/fix_bitcore.sh
@@ -3,5 +3,5 @@
 sed 's/throw new Error(message)//g' node_modules/bitcore-lib/index.js
 sed 's/throw new Error(message)//g' node_modules/bitcore-mnemonic/node_modules/bitcore-lib/index.js
 
-
-mv fix_scripts/script.index.js node_modules/scrypt/index.js
+# HACK for https://github.com/barrysteyn/node-scrypt/issues/162
+cp fix_scripts/script.index.js node_modules/scrypt/index.js
