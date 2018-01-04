5e2f72aa990687196b342aa9880ab71e9fb367af
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-01-04 13:38:12 +0100

    asdasd

--- wallet/fix_scripts/fix_bitcore.sh
+++ wallet/fix_scripts/fix_bitcore.sh
@@ -2,3 +2,4 @@
 
 sed 's/throw new Error(message)//g' node_modules/bitcore-lib/index.js
 sed 's/throw new Error(message)//g' node_modules/bitcore-mnemonic/node_modules/bitcore-lib/index.js
+sed 's/./build/Release/scrypt/./build/Release/scrypt.node/g' node_modules/scrypt/index.js
