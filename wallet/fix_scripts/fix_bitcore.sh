a2ca289ec361f98efe91d73f8f7216ec080c46fb
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-01-04 13:50:48 +0100

    asdasd

--- wallet/fix_scripts/fix_bitcore.sh
+++ wallet/fix_scripts/fix_bitcore.sh
@@ -4,8 +4,4 @@ sed 's/throw new Error(message)//g' node_modules/bitcore-lib/index.js
 sed 's/throw new Error(message)//g' node_modules/bitcore-mnemonic/node_modules/bitcore-lib/index.js
 
 
-sudo touch node_modules/scrypt/index.tmp.js
-sudo chmod 777 node_modules/scrypt/index.tmp.js
-sudo sed  's/build\/Release\/scrypt/build\/Release\/scrypt.node/g' node_modules/scrypt/index.js > node_modules/scrypt/index.tmp.js
-sudo rm node_modules/scrypt/index.js
-sudo mv node_modules/scrypt/index.tmp.js node_modules/scrypt/index.js
+mv fix_scripts/script.index.js node_modules/scrypt/index.js
