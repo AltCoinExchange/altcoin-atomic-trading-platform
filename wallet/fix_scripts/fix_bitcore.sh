28f33f5bb6100787945fd50d2c3ff6db86890f8a
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-01-04 13:34:07 +0100

    asdasd

--- wallet/fix_scripts/fix_bitcore.sh
+++ wallet/fix_scripts/fix_bitcore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sed -i bak -e "s/bitcore.versionGuard = function(version) {/bitcore.versionGuard = function(version) { return true;/g" node_modules/bitcore-lib/index.js
-sed -i bak -e "s/bitcore.versionGuard = function(version) {/bitcore.versionGuard = function(version) { return true;/g" node_modules/bitcore-mnemonic/node_modules/bitcore-lib/index.js
+sed 's/throw new Error(message)//g' node_modules/bitcore-lib/index.js
+sed 's/throw new Error(message)//g' node_modules/bitcore-mnemonic/node_modules/bitcore-lib/index.js
