7d174c0050c924e02dafdd97c294fa800b87f539
Azra <azramah6@hotmail.com>
2018-01-05 17:46:01 +0100

    token balances on swap

--- shapeshift/src/app/swap/swap-start/swap-start.component.ts
+++ shapeshift/src/app/swap/swap-start/swap-start.component.ts
@@ -13,6 +13,29 @@ import {MatDialog} from "@angular/material";
 import {AllCoinsDialogComponent} from "../../common/coins-dialog/all-coins.dialog";
 import {AppState} from "../../reducers/app.state";
 import {Observable} from "rxjs/Observable";
+import {WalletRecord} from "../../reducers/balance.reducer";
+import {TOKENS} from "altcoinio-wallet";
+import {
+  getBTCBalance,
+  getETHBalance,
+  getTokenBalanceAragon,
+  getTokenBalanceAugur,
+  getTokenBalanceBat,
+  getTokenBalanceBytom,
+  getTokenBalanceCivic,
+  getTokenBalanceDent,
+  getTokenBalanceDistrict0x,
+  getTokenBalanceEos,
+  getTokenBalanceGnosis,
+  getTokenBalanceGolem,
+  getTokenBalanceOmiseGo,
+  getTokenBalances,
+  getTokenBalanceSalt,
+  getTokenBalanceStatusNetwork,
+  getTokenBalanceSubstratum,
+  getTokenBalanceTron
+} from "../../selectors/balance.selector";
+import {GetBtcBalanceAction, GetEthBalanceAction, GetTokenBalanceAction} from "../../actions/balance.action";
 
 @Component({
   selector: "app-swap-start",
@@ -35,6 +58,23 @@ export class SwapStartComponent extends AnimationEnabledComponent implements OnI
   $quote: Observable<number>;
   $depositUSD: Observable<number>;
   $receiveUSD: Observable<number>;
+  $tokenBalanceAugur: Observable<WalletRecord>;
+  $tokenBalanceGolem: Observable<WalletRecord>;
+  $tokenBalanceAragon: Observable<WalletRecord>;
+  $tokenBalanceEos: Observable<WalletRecord>;
+  $tokenBalanceSalt: Observable<WalletRecord>;
+  $tokenBalanceBat: Observable<WalletRecord>;
+  $tokenBalanceGnosis: Observable<WalletRecord>;
+  $tokenBalanceCivic: Observable<WalletRecord>;
+  $tokenBalanceDistrict0x: Observable<WalletRecord>;
+  $tokenBalanceStatusNetwork: Observable<WalletRecord>;
+  $tokenBalanceSubstratum: Observable<WalletRecord>;
+  $tokenBalanceTron: Observable<WalletRecord>;
+  $tokenBalanceBytom: Observable<WalletRecord>;
+  $tokenBalanceDent: Observable<WalletRecord>;
+  $tokenBalanceOmiseGo: Observable<WalletRecord>;
+  $ethBalance: Observable<WalletRecord>;
+  $btcBalance: Observable<WalletRecord>;
 
   constructor(private store: Store<AppState>, public dialog: MatDialog) {
     super();
@@ -103,6 +143,9 @@ export class SwapStartComponent extends AnimationEnabledComponent implements OnI
         return price;
       }
     );
+
+    this.getTokenBalances();
+    this.getTokenAmountUSD();
   }
 
   ngOnInit() {
@@ -160,6 +203,127 @@ export class SwapStartComponent extends AnimationEnabledComponent implements OnI
       this.store.dispatch(new swapAction.setDepositCoinAction(coin));
     else
       this.store.dispatch(new swapAction.setReceiveCoinAction(coin));
+  }
+
+  getTokenBalances() {
+
+    this.store.dispatch(new GetEthBalanceAction());
+    this.store.dispatch(new GetBtcBalanceAction());
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.GOLEM, name: "golem"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.AUGUR, name: "augur"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.ARAGON, name: "aragon"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.BAT, name: "bat"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.EOS, name: "eos"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.GNOSIS, name: "gnosis"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.SALT, name: "salt"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.CIVIC, name: "civic"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.OMISEGO, name: "omisego"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.DISTRICT0X, name: "district0x"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.STATUSNETWORK, name: "statusnetwork"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.SUBSTRATUM, name: "substratum"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.TRON, name: "tron"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.BYTOM, name: "bytom"}));
+    this.store.dispatch(new GetTokenBalanceAction({token: TOKENS.DENT, name: "dent"}));
+
+    this.$ethBalance = this.store.select(getETHBalance);
+    this.$btcBalance = this.store.select(getBTCBalance);
+    this.$tokenBalanceAugur = this.store.select(getTokenBalanceAugur);
+    this.$tokenBalanceGolem = this.store.select(getTokenBalanceGolem);
+    this.$tokenBalanceAragon = this.store.select(getTokenBalanceAragon);
+    this.$tokenBalanceBat = this.store.select(getTokenBalanceBat);
+    this.$tokenBalanceEos = this.store.select(getTokenBalanceEos);
+    this.$tokenBalanceGnosis = this.store.select(getTokenBalanceGnosis);
+    this.$tokenBalanceSalt = this.store.select(getTokenBalanceSalt);
+
+    this.$tokenBalanceCivic = this.store.select(getTokenBalanceCivic);
+    this.$tokenBalanceOmiseGo = this.store.select(getTokenBalanceOmiseGo);
+    this.$tokenBalanceDistrict0x = this.store.select(getTokenBalanceDistrict0x);
+    this.$tokenBalanceStatusNetwork = this.store.select(getTokenBalanceStatusNetwork);
+    this.$tokenBalanceSubstratum = this.store.select(getTokenBalanceSubstratum);
+    this.$tokenBalanceTron = this.store.select(getTokenBalanceTron);
+    this.$tokenBalanceBytom = this.store.select(getTokenBalanceBytom);
+    this.$tokenBalanceDent = this.store.select(getTokenBalanceDent);
+
+    const tokenBalances = this.store.select(getTokenBalances);
+    //console.log(tokenBalances); // todo can be done better
+
+    this.coins.forEach((coin) => {
+      switch (coin.name) {
+        case "BTC":
+          coin.$balance = this.$btcBalance;
+          this.selectedCoin = coin;
+          break;
+        case "ETH":
+          coin.$balance = this.$ethBalance;
+          break;
+        case "REP":
+          coin.$balance = this.$tokenBalanceAugur;
+          break;
+        case "GNT":
+          coin.$balance = this.$tokenBalanceGolem;
+          break;
+        case "GNO":
+          coin.$balance = this.$tokenBalanceGnosis;
+          break;
+        case "BAT":
+          coin.$balance = this.$tokenBalanceBat;
+          break;
+        case "ANT":
+          coin.$balance = this.$tokenBalanceAragon;
+          break;
+        case "EOS":
+          coin.$balance = this.$tokenBalanceEos;
+          break;
+        case "SALT":
+          coin.$balance = this.$tokenBalanceSalt;
+          break;
+        case "CVC":
+          coin.$balance = this.$tokenBalanceCivic;
+          break;
+        case "OMG":
+          coin.$balance = this.$tokenBalanceOmiseGo;
+          break;
+        case "DNT":
+          coin.$balance = this.$tokenBalanceDistrict0x;
+          break;
+        case "SNT":
+          coin.$balance = this.$tokenBalanceStatusNetwork;
+          break;
+        case "SUB":
+          coin.$balance = this.$tokenBalanceSubstratum;
+          break;
+        case "TRN":
+          coin.$balance = this.$tokenBalanceTron;
+          break;
+        case "BTM":
+          coin.$balance = this.$tokenBalanceBytom;
+          break;
+        case "DENT":
+          coin.$balance = this.$tokenBalanceDent;
+          break;
+        default:
+          coin.$balance = this.$tokenBalanceDent;
+      }
+
+    });
+  }
+
+  getTokenAmountUSD() {
+    const quotes = this.store.select(quoteSelector.getQuotes);
+    this.coins.forEach((coin) => {
+      coin.$amountUSD = Observable.combineLatest(quotes, coin.$balance, (q, coinBalance) => {
+        if (!q || !coinBalance)
+          return undefined;
+        const balance = Number(coinBalance.balance);
+        const coinQuote = q.get(coin.name);
+        const number = balance * coinQuote.price;
+        const price = +number.toFixed(2);
+        if (isNaN(number)) {
+          return 0;
+        }
+        return price;
+      });
+    });
   }
 
 }
