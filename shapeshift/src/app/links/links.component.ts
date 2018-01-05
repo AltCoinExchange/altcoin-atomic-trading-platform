83f0de38a09b772ad7d93c04147f78a5ba0239b0
Azra <azramah6@hotmail.com>
2018-01-05 17:36:27 +0100

    change faq link

--- shapeshift/src/app/links/links.component.ts
+++ shapeshift/src/app/links/links.component.ts
@@ -12,7 +12,7 @@ export class LinksComponent implements OnInit {
   constructor() {
     this.links = [
       {name: 'Blog', icon: 'fa fa-file-text-o', url: 'https://blog.altcoin.io/'},
-      {name: 'FAQ', icon: 'fa fa-question-circle-o', url: 'https://blog.altcoin.io/worlds-first-atomic-swap-wallet-from-altcoin-io-released-6f1cfc52d1cc'},
+      {name: 'FAQ', icon: 'fa fa-question-circle-o', url: 'https://blog.altcoin.io/atomic-swap-wallet-beta-release-b6e3eef55842'},
       {name: 'Twitter', icon: 'fa fa-twitter', url: 'https://twitter.com/altcoin_io'},
       {name: 'Telegram', icon: 'fa fa-telegram', url: 'https://t.me/altcoinexchange'},
       {name: 'Contact', icon: 'fa fa-envelope-o', url: 'mailto:support@altcoin.io'},
