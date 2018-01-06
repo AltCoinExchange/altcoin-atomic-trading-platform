607946db4af60ae5fc461020ed4e6f4e4a410e9e
Admir Sabanovic <admir.sabanovic@moduscreate.com>
2018-01-06 14:37:51 +0100

    test http2 protocol

--- shapeshift/src/index.js
+++ shapeshift/src/index.js
@@ -1,21 +1,81 @@
-'use strict';
+// 'use strict';
+//
+// var compression = require('compression');
+// var express = require('express');
+// var app = express();
+//
+// // compress all requests
+// app.use(compression());
+//
+// app.use(express.static(__dirname));
+//
+// app.get('/*', function (req, res) {
+//   res.sendFile(__dirname + '/index.html');
+// });
+//
+// var server = app.listen(3333, function () {
+//   var host = server.address().address;
+//   var port = server.address().port;
+//
+//   console.log('ALTCOIN.IO listening at http://%s:%s', host, port);
+// });
 
-var compression = require('compression');
-var express = require('express');
-var app = express();
+var fs = require('fs');
+const hapi = require('hapi');
+const Inert = require('inert');
+const Path = require('path');
 
-// compress all requests
-app.use(compression());
+var http2 = require('http2');
+var options = {
+  key: fs.readFileSync(__dirname + '/server.key'),
+  cert: fs.readFileSync(__dirname + '/server.crt'),
+};
 
-app.use(express.static(__dirname));
 
-app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+const server = new hapi.Server({
+  listener: http2.createSecureServer(options),
+  host: 'localhost',
+  port: 3000,
+  tls: true
 });
 
-var server = app.listen(3333, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('ALTCOIN.IO listening at http://%s:%s', host, port);
-});
+const provision = async () => {
+
+  await server.register(Inert);
+
+  server.route({
+    method: 'GET',
+    path: '/{p*}',
+    handler: {
+      directory: {
+        path: '.',
+        redirectToSlash: true,
+        lookupCompressed: true,
+        index: true,
+      }
+    }
+  });
+
+  server.ext('onPreResponse', (request, reply) => {
+    const response = request.response;
+
+    if (!response.isBoom) {
+      return reply.file(request.response.source.path);
+    }
+
+    // else an error has occurred
+    const error = response;
+
+    // if the error is 'Object not found', call index.html
+    if (error.output.statusCode === 404) {
+      return reply.file('index.html');
+    }
+  });
+
+  await server.start();
+
+  console.log('Server running at:', server.info.uri);
+};
+
+provision();
