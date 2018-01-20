8f1944e9538ad730957fa53fa607ef430fb4b661
MisterDr <djenadrazic@gmail.com>
2018-01-20 16:50:50 +0100

    - Added chart component

--- altcoinio/src/app/tokens/token-preview/token-preview.component.ts
+++ altcoinio/src/app/tokens/token-preview/token-preview.component.ts
@@ -8,9 +8,49 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TokenPreviewComponent implements OnInit {
   @Input() token;
 
+  public single: any[];
+  public multi: any[] = [
+    {
+      name: 'BTC',
+      series: [
+        {
+          name: '1990',
+          value: 0
+        },
+        {
+          name: '2000',
+          value: 1200
+        },
+        {
+          name: '2010',
+          value: 11000
+        },
+      ]
+    }];
+
+  public view: any[] = [200, 100];
+
+  // options
+  public showXAxis = false;
+  public scaleType = "linear";
+  public showYAxis = false;
+  public gradient = false;
+  public showLegend = true;
+  public showXAxisLabel = false;
+  public xAxisLabel = 'Coin';
+  public showYAxisLabel = false;
+  public yAxisLabel = 'Price';
+
+  public colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(event) {
+    console.log(event);
+  }
 }
--- altcoinio/src/app/tokens/tokens.module.ts
+++ altcoinio/src/app/tokens/tokens.module.ts
@@ -4,12 +4,15 @@ import {TokenPreviewComponent} from "./token-preview/token-preview.component";
 import {TokenListComponent} from "./token-list/token-list.component";
 import {AltcoinIoCommonModule} from "../common/common.module";
 import {FlexLayoutModule} from "@angular/flex-layout";
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+
 
 @NgModule({
   imports: [
     CommonModule,
     AltcoinIoCommonModule,
     FlexLayoutModule,
+    NgxChartsModule,
   ],
   declarations: [
     TokenPreviewComponent,
